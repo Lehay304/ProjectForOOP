3b21a6a1cacbdacb3e377cb874814c96d609af90
Lehay304 <Alecsey.vip.g@gmail.com>
2025-01-20 01:40:03 +0700

    Побаловался с библиотекой Tkinter

--- Project.py
+++ Project.py
@@ -1,1 +1,35 @@
-print(2+2)
+import tkinter as tk
+from tkinter import *
+from tkinter import ttk
+
+root = tk.Tk()
+root.title("Отель Handsome")
+root.geometry("400x450")
+
+counts = 0
+
+def vhod():
+    counts =+ 1
+    global labalg
+    labalg["text"] = f"Вход не удался {counts} раз"
+
+btnsend = ttk.Button(text="Вход", command=vhod)
+btnsend.place(width=70, height=40, x=170, y=330)
+
+
+labalg = ttk.Label(text="")
+labalg.pack()
+
+enterlogin = ttk.Entry()
+enterlogin.place(width=200, height=30,)
+
+root.mainloop()
+
+class Hotel:
+    def __init__(self, customer, room, employee, service, hotelsystem):
+        self.customer = customer
+        self.room = room
+        self.employee = employee
+        self.service = service
+        self.hotelsystem = hotelsystem
+
